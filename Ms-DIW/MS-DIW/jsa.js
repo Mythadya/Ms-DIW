6c71506d5650705224b5e835858c76f6b873b74b
Mythadya <162562454+Mythadya@users.noreply.github.com>
2024-04-30 15:20:34 +0200

    Add files via upload

--- Ms-DIW/MS-DIW/jsa.js
+++ Ms-DIW/MS-DIW/jsa.js
@@ -1,8 +1,13 @@
-window.addEventListener('scroll', function() {
+/* window.addEventListener('scroll', function() {
     var card = document.querySelector('.parallax-card');
     if (window.pageYOffset > 100) {
       card.classList.add('scrolled');
     } else {
       card.classList.remove('scrolled');
     }
-  });
+  }); */
+/*   document.querySelectorAll('.navbar-nav .nav-link').forEach(function(element) {
+    element.addEventListener('click', function() {
+        document.querySelector('.navbar-collapse').classList.remove('show');
+    });
+}); */
