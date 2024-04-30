c91b9cb02530d0eef492b6f203eaecec1638705c
Mythadya <162562454+Mythadya@users.noreply.github.com>
2024-04-30 08:53:07 +0200

    Delete MS-DIW directory

--- MS-DIW/jsa.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    var card = document.querySelector('.parallax-card');
-    if (window.pageYOffset > 100) {
-      card.classList.add('scrolled');
-    } else {
-      card.classList.remove('scrolled');
-    }
-  });
