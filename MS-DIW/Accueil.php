51a27885cdb6bdc0efafc5c07402608d2da0b70e
Mythadya <162562454+Mythadya@users.noreply.github.com>
2024-06-06 08:36:06 +0200

    Add files via upload

---
+++ MS-DIW/Accueil.php
@@ -0,0 +1,100 @@
+<?php include 'header.php'; ?>
+
+<main>
+ <div class="container">
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+            <div class="col">
+                <div class="card">
+                    <a href="../MS-DIW/Catégorie_pizza.php">
+                        <img src="../MS-DIW/images_the_district (1)/category/pizza_cat.jpg" class="card-img-top"
+                            alt="pizza_cat">
+                        <div class="card-body" style="background-color: #333; color: #fff;">
+                            <h5 class="card-title">Pizza</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <a href="../MS-DIW/Catégorie_burger.php">
+                        <img src="../MS-DIW/images_the_district (1)/category/burger_cat.jpg" class="card-img-top"
+                            alt="burger_cat">
+                        <div class="card-body" style="background-color: #333; color: #fff;">
+                            <h5 class="card-title">Burger</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <a href="../MS-DIW/Catégorie_pate.php">
+                        <img src="../MS-DIW/images_the_district (1)/category/pasta_cat.jpg" class="card-img-top"
+                            alt="pasta_cat">
+                        <div class="card-body" style="background-color: #333; color: #fff;">
+                            <h5 class="card-title">Pate</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <a href="../MS-DIW/Catégorie_asiatique.php">
+                        <img src="../MS-DIW/images_the_district (1)/category/asian_food_cat.jpg" class="card-img-top"
+                            alt="asian_food_cat" height="410px">
+                        <div class="card-body" style="background-color: #333; color: #fff;">
+                            <h5 class="card-title">Asiatique</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <a href="../MS-DIW/Catégorie_Salade.php">
+                        <img src="../MS-DIW/images_the_district (1)/category/salade_cat.jpg" class="card-img-top"
+                            alt="salade_cat" height="410px">
+                        <div class="card-body" style="background-color: #333; color: #fff;">
+                            <h5 class="card-title">Salade</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <a href="../MS-DIW/Catégorie_Sandwich.php">
+                        <img src="../MS-DIW/images_the_district (1)/category/sandwich_cat.jpg" class="card-img-top"
+                            alt="sandwich_cat" height="410px">
+                        <div class="card-body" style="background-color: #333; color: #fff;">
+                            <h5 class="card-title">Sandwich</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div><br><br>
+    <div class="container galerie-images">
+        <h2>Galerie D'images de Plats</h2><br>
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 66%;">
+                    <img src="../MS-DIW/images_the_district (1)/food/Food-Name-6340.jpg" class="card-img-top"
+                        alt="Food-Name-6340">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 79% ;">
+                    <img src="../MS-DIW/images_the_district (1)/food/spaghetti-legumes.jpg" class="card-img-top"
+                        alt="spaghetti-legumes">
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 88%;">
+                    <img src="../MS-DIW/images_the_district (1)/food/pizza-margherita.jpg" class="card-img-top"
+                        alt="pizza-margherita">
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
+</main>
+
+<?php include 'footer.php'; ?>
---
+++ MS-DIW/Catégorie_burger.php
@@ -0,0 +1,48 @@
+<?php include 'header.php'; ?>
+
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="../MS-DIW/images_the_district (1)/food/cheesburger.jpg" class="card-img img-fluid"
+                                alt="cheesburger" style="max-width: 200px; height: 220px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Cheeseburger</h5>
+                                <p class="card-text">Ceci est une description du produit 1. Lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit.</p><br><br>
+                                <a href="../MS-DIW/Commande.php"
+                                    class="btn btn-primary d-flex justify-content-end">Commander</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="../MS-DIW/images_the_district (1)/food/hamburger.jpg" class="card-img img-fluid"
+                                alt="Hamburger" style="max-width: 300px; height: 220px;">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Hamburger</h5>
+                                <p class="card-text">Ceci est une description du produit 2. Lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit.</p><br><br>
+                                <a href="lien_produit_2"
+                                    class="btn btn-primary d-flex justify-content-end">Commander</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><br>
+</main>
+
+<?php include 'footer.php'; ?>
